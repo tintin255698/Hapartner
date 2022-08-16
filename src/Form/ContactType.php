c4b36a3c6c112a3d6ddf8b32cab0cb18a4657254
tintin255698 <vivien.joly@hotmail.fr>
2022-08-16 20:34:59 +0200

    Hapartner

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -12,14 +12,23 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ContactType extends AbstractType
 {
+
+    public $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName', TextType::class, ['label' => 'Prénom', 'attr'=>['placeholder'=>'Votre prénom']])
-            ->add('name', TextType::class, ['label' => 'Nom',   'attr'=>['placeholder'=>'Votre nom']])
+            ->add('firstName', TextType::class)
+            ->add('name', TextType::class)
             ->add('business', TextType::class, ['label' => 'Entreprise<span class="badge rounded-pill bg-success">Facultatif</span>','label_html'=>true, 'required' => false,  'attr'=>['placeholder'=>'Votre entreprise']])
             ->add('email', EmailType::class, ['label' => 'Email',   'attr'=>['placeholder'=>'Votre email']] )
             ->add('telephone', TelType::class, ['label' => 'Téléphone',  'attr'=>['placeholder'=>'Votre téléphone']])
@@ -30,7 +39,7 @@ class ContactType extends AbstractType
                     'Support Opérationnel' => 'Support Opérationnel',
                     'Support Contractuel' => 'Support Contractuel',
                     'Autre' => 'Autre',
-                ]])
+                ] , 'translation_domain' => 'contact' ])
             ->add('content', TextareaType::class)
             ->add('envoyer', SubmitType::class,  ['row_attr' => [
                 'class' => 'text-end']])
